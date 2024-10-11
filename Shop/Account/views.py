61a90487645000c4f0bd2222a070f85759eebe62
Koosha-bit <kooshakazemig@gmail.com>
2024-10-12 00:54:46 +0330

    create view for user registering

--- Shop/Account/views.py
+++ Shop/Account/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializer import RegisterUserSerializer
+
+User = get_user_model()
+
+class RegisterUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        refresh_token = RefreshToken.for_user(request.user)
+
+
+        return Response({
+                'refresh_token' : str(refresh_token),
+                'access_token' : str(refresh_token.access_token)
+            })
