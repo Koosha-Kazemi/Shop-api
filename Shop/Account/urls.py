6c24168c5d86a4a30451dc6f2e95579ae0f8953a
Koosha-bit <kooshakazemig@gmail.com>
2024-10-12 01:11:33 +0330

    create url for user registering

--- Shop/Account/urls.py
+++ Shop/Account/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView
 )
 
+from .views import RegisterUserView
+
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/',RegisterUserView.as_view(), name='register_user')
 ]
