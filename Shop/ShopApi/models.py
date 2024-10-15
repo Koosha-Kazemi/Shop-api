2669ec5754de8b7f96c626e6029356f8794126b6
Koosha-bit <kooshakazemig@gmail.com>
2024-10-15 19:43:49 +0330

    create Product model

--- Shop/ShopApi/models.py
+++ Shop/ShopApi/models.py
@@ -6,9 +6,18 @@ class Category(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+
+
 class Product(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField(max_length=500)
     price = models.PositiveIntegerField()
     discount = models.PositiveIntegerField()
     stock = models.BooleanField()
+
+    def __str__(self):
+        return f'name : {self.name} price : {self.price}  discount : {self.discount} stock : {self.stock}'
+
+
+
+
