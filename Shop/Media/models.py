fa927e5e6e801a861658e34e0a54a8cd65ab346d
Koosha-bit <kooshakazemig@gmail.com>
2024-10-17 12:19:40 +0330

    create Image model

--- Shop/Media/models.py
+++ Shop/Media/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/', width_field='width', height_field='height')
+    width = models.IntegerField(editable=False)
+    height = models.IntegerField(editable=False)
